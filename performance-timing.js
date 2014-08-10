5ef79d4d16937a335b97f57f10cfcf48d6bdfa55
Chris Blakley <chrisb@pinckneyhugo.com>
2014-08-10 00:56:15 -0400

    Fixed 'undefined' issue

--- performance-timing.js
+++ performance-timing.js
@@ -6,14 +6,14 @@ window.onload = function(){
 		} else {
 			pageSpeed[0] = 'Redirections: ' + performance.navigation.redirectCount + ' @ ' + (performance.timing.redirectEnd-performance.timing.redirectStart)/1000 + 's';
 		}
-		pageSpeed[2] = 'App Cache Time: ' + (performance.timing.domainLookupStart-performance.timing.fetchStart)/1000 + 's';
-		pageSpeed[3] = 'DNS Time: ' + (performance.timing.domainLookupEnd-performance.timing.domainLookupStart)/1000 + 's';
-		pageSpeed[4] = 'TCP Time: ' + (performance.timing.connectEnd-performance.timing.connectStart)/1000 + 's';
-		pageSpeed[5] = 'Request Time: ' + (performance.timing.responseStart-performance.timing.requestStart)/1000 + 's';
-		pageSpeed[6] = 'Response Time: ' + (performance.timing.responseEnd-performance.timing.responseStart)/1000 + 's';
-		pageSpeed[7] = 'DOM Time: ' + (performance.timing.domComplete-performance.timing.domLoading)/1000 + 's';
-		pageSpeed[8] = 'onLoad Time: ' + (performance.timing.loadEventEnd-performance.timing.loadEventStart)/1000 + 's';
-		pageSpeed[9] = 'Total Page Load Time: ' + (performance.timing.loadEventEnd-performance.timing.navigationStart)/1000 + 's';
+		pageSpeed[1] = 'App Cache Time: ' + (performance.timing.domainLookupStart-performance.timing.fetchStart)/1000 + 's';
+		pageSpeed[2] = 'DNS Time: ' + (performance.timing.domainLookupEnd-performance.timing.domainLookupStart)/1000 + 's';
+		pageSpeed[3] = 'TCP Time: ' + (performance.timing.connectEnd-performance.timing.connectStart)/1000 + 's';
+		pageSpeed[4] = 'Request Time: ' + (performance.timing.responseStart-performance.timing.requestStart)/1000 + 's';
+		pageSpeed[5] = 'Response Time: ' + (performance.timing.responseEnd-performance.timing.responseStart)/1000 + 's';
+		pageSpeed[6] = 'DOM Time: ' + (performance.timing.domComplete-performance.timing.domLoading)/1000 + 's';
+		pageSpeed[7] = 'onLoad Time: ' + (performance.timing.loadEventEnd-performance.timing.loadEventStart)/1000 + 's';
+		pageSpeed[8] = 'Total Page Load Time: ' + (performance.timing.loadEventEnd-performance.timing.navigationStart)/1000 + 's';
 		
 		console.log('******* BEGIN SPEED TEST RESULTS *******');	
 		for (var i = 0; i < pageSpeed.length; i++) {
